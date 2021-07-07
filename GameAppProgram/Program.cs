0e523401e30d5ccbf91562b5e8c24758424ca91e
navyashrees29 <navya9529@gmail.com>
2021-07-07 16:04:26 +0530

    Upload

---
+++ GameAppProgram/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameAppProgram
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Word Guess Game");
+            string displayMessage = $"Enter Your Choice {(int)Options.BASIC}->Basic,{(int)Options.INTERMEDIATE}->Intermediate,{(int)Options.ADVANCED}->Advanced";
+            Console.WriteLine(displayMessage);
+            Options _choice = (Options)Int32.Parse(Console.ReadLine());
+
+            WordGuessGame game = new WordGuessGame();
+            game.StartGame(_choice);
+
+        }
+    }
+}
