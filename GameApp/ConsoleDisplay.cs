bc991bfca7a916bed23ad588ca1d2a5e7cc04d81
navyashrees29 <navya9529@gmail.com>
2021-07-08 10:04:18 +0530

    update

--- GameApp/ConsoleDisplay.cs
+++ GameApp/ConsoleDisplay.cs
@@ -4,21 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GameAppProgram
+namespace GameApp
 {
-   
-    class Program
+    class ConsoleDisplay
     {
-        static void Main(string[] args)
+        public void Display()
         {
             Console.WriteLine("Word Guess Game");
             string displayMessage = $"Enter Your Choice {(int)Options.BASIC}->Basic,{(int)Options.INTERMEDIATE}->Intermediate,{(int)Options.ADVANCED}->Advanced";
             Console.WriteLine(displayMessage);
-            Options _choice = (Options)Int32.Parse(Console.ReadLine());
-
-            WordGuessGame game = new WordGuessGame();
-            game.StartGame(_choice);
-
         }
     }
 }
