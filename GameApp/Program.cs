9efe3996b0aadfd4c6dd260abbda7ed7a0006b65
navyashrees29 <navya9529@gmail.com>
2021-07-13 11:29:22 +0530

    update

--- GameApp/Program.cs
+++ GameApp/Program.cs
@@ -6,23 +6,62 @@ using System.Threading.Tasks;
 
 namespace GameApp
 {
- 
     public enum Options
     {
-        BASIC=1,INTERMEDIATE,ADVANCED
+        BASIC = 1, INTERMEDIATE, ADVANCED
     }
-    class Program
-    {
-        static void Main(string[] args)
+
+   
+        class Program
         {
-            ConsoleDisplay consoleDisplay = new ConsoleDisplay();
-            consoleDisplay.Display();
+            static void Main(string[] args)
+            {
+            int count = 3;
+            do
+            {
+                Console.WriteLine("Word Guess Game");
+                string displayMessage = $"Enter Your Choice {(int)Options.BASIC}->Basic,{(int)Options.INTERMEDIATE}->Intermediate,{(int)Options.ADVANCED}->Advanced";
+                Console.WriteLine(displayMessage);
 
-            ValidateUserInputs validateUserInputs = new ValidateUserInputs();
-            Options value= validateUserInputs.ValidateInputs();
+                try
+                {
+                    Options _choice = (Options)Int32.Parse(Console.ReadLine());
 
-            CheckSwitchCondition checkCondition = new CheckSwitchCondition();
-            checkCondition.SwitchCondition(value);
+                    LevelLib.LevelType levelType = new LevelLib.LevelType();
+
+                    if (_choice != Options.BASIC && _choice != Options.INTERMEDIATE && _choice != Options.ADVANCED)
+                    {
+                        Console.WriteLine("Please enter valid number");
+
+                    }
+                    else
+                    {
+                        switch (_choice)
+                        {
+                            case Options.BASIC:
+                                Console.WriteLine("Basic Level");
+                                levelType.Level(@"C:\Users\navyashrees\Documents\GitHub\latebinding-using-reflection-navyashrees29\GameApp\bin\Debug\LevelLibs\BasicLevelLib.dll", "BasicLevelLib.BasicLevelType", "Play");
+                                break;
+
+                            case Options.INTERMEDIATE:
+                                Console.WriteLine("Intermediate Level");
+                                levelType.Level(@"C:\Users\navyashrees\Documents\GitHub\latebinding-using-reflection-navyashrees29\GameApp\bin\Debug\LevelLibs\IntermediateLevelLib.dll", "IntermediateLevelLib.IntermediateLevelType", "Start");
+                                break;
+
+                            case Options.ADVANCED:
+                                Console.WriteLine("Advanced Level");
+                                levelType.Level(@"C:\Users\navyashrees\Documents\GitHub\latebinding-using-reflection-navyashrees29\GameApp\bin\Debug\LevelLibs\AdvancedLevelLib.dll", "AdvancedLevelLib.AdvancedLevelType", "Begin");
+                                break;
+                        }
+                        break;
+                    }
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Enter integer value");
+                }
+                --count;
+            } while (count > 0);
         }
     }
 }
---
+++ LevelLib/LevelType.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelLib
+{
+    public class LevelType
+    {
+        public void Level(string getPath, string getType, string getMethod)
+        {
+            System.Reflection.Assembly levelLib = System.Reflection.Assembly.LoadFile(getPath);
+            System.Type allLevelTypeClassRef = FindClass(levelLib, getType);
+            Object objRef = System.Activator.CreateInstance(allLevelTypeClassRef);
+            System.Reflection.MethodInfo methodRef = allLevelTypeClassRef.GetMethod(getMethod);
+            if (getMethod == "Play")
+            {
+                object result = InvokeMethod(objRef, methodRef, new object[] { });
+                Console.WriteLine(result.ToString());
+            }
+            else if (getMethod == "Start")
+            {
+                object result = InvokeMethod(objRef, methodRef, new object[] { "Navya" });
+                Console.WriteLine(result.ToString());
+            }
+            else if (getMethod == "Begin")
+            {
+                object result = InvokeMethod(objRef, methodRef, new object[] { "Navya", 100 });
+                Console.WriteLine(result.ToString());
+            }
+        }
+
+        System.Type FindClass(System.Reflection.Assembly assemblyRef, String getType)
+        {
+            System.Type classType = assemblyRef.GetType(getType);
+            if (classType != null)
+            {
+                if (classType.IsClass)
+                {
+                    return classType;
+                }
+            }
+            return null;
+        }
+
+        object InvokeMethod(object source, System.Reflection.MethodInfo methodRef, object[] arguments)
+        {
+            return methodRef.Invoke(source, arguments);
+        }
+    }
+}
